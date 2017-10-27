50856cdd6893fb04b56361d131a37c2bdbadd260
Daniel Bearden <karthos@users.noreply.github.com>
2017-10-26 19:37:16 -0700

    Cleaned up namespaces

--- BearDenFileStorage/Services/ConfigurationMessageService.cs
+++ BearDenFileStorage/Services/ConfigurationMessageService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BearDenFileStorage;
 using Microsoft.Extensions.Configuration;
 
 namespace BearDenFileStorage
--- BearDenFileStorage/Startup.cs
+++ BearDenFileStorage/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-using BearDenFileStorage;
 using Microsoft.AspNetCore.Routing;
 
 namespace BearDenFileStorage
